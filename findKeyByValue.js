9abb770841a81c900e7f289e3954b7c524cbdeb9
mhersi27 <138933481+mhersi27@users.noreply.github.com>
2023-09-29 12:39:00 -0400

    Update findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,17 +1,9 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+const findKeyByValue = function(object, value){
+  for (key in object){
+    if (object[key] === value)
+      return key
   }
-};
+  return undefined
+}
 
-assertEqual("boy", true);
-
-const findKeyByValue = function (item, value) {
-  for (const key in item) {
-    if (item[key] === value) {
-      return key;
-    }
-  }
-};
+module.exports = findKeyByValue;
