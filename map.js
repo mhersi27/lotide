9e344134c5f83eaac8d228a7bba29cf0ba95111e
mhersi27 <138933481+mhersi27@users.noreply.github.com>
2023-09-29 12:36:21 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,7 +1,3 @@
-// const words = ["ground", "control", "to", "major", "tom"];
-// const results1 = map(words, (word) => word[0]);
-// console.log(results1);
-
 const map = function (array, callback) {
   const results = [];
   array.forEach((item) => {
